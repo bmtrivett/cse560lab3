14b408795d5707b53d467958886af7a93a8cad86
dmglouis@gmail.com <dmglouis@gmail.com@339cb73f-8dcb-adfe-f73e-b3236ebafd8d>
2011-02-22 22:47:17 +0000

    Commented second pass so that its easy to uncomment (Shift-Ctrl-C).

--- src/WilevenAssembler.java
+++ src/WilevenAssembler.java
@@ -29,15 +29,15 @@ public class WilevenAssembler {
 		}
 
 		// Execute pass two.
-		//String secondPassError = PassTwo.output(fileNames[1], fileNames[2],
-				//machineTables);
-
-		// If the second pass ended abruptly and returned an error, display it
-		// and exit.
-		//if (secondPassError.length() > 0) {
-			//System.out.println("ERROR: " + secondPassError);
-			//System.exit(0);
-		//}
+//		String secondPassError = PassTwo.output(fileNames[1], fileNames[2],
+//				machineTables);
+//
+//		// If the second pass ended abruptly and returned an error, display it
+//		// and exit.
+//		if (secondPassError.length() > 0) {
+//			System.out.println("ERROR: " + secondPassError);
+//			System.exit(0);
+//		}
 	}
 
 	/**
